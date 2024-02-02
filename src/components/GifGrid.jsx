d30152eac5863e1152f0661421c7c9405c4dcf40
Sergioig37 <sergioig37@educastur.es>
2024-02-02 12:25:26 +0100

    s

--- src/components/GifGrid.jsx
+++ src/components/GifGrid.jsx
@@ -3,14 +3,26 @@ import React from "react";
 export const GifGrid = ({categoria}) => {
 
 
-  //asincrono : que el programa manda petición y espera a que llegue la respuesta
-  //sincrono: sigue incluso si no responde
+  //asincrono :  que el programa manda petición y espera a que llegue la respuesta
+  //sincrono:  sigue incluso si no responde
   const getGifs = async () =>{
     const url = "https://api.giphy.com/v1/gifs/search?api_key=5Px5eEfD8XwSw6EBYY84ywknPkqEau6m&q=cheeseburgers&limit=10"
     //fetch es una función para buscar
     const respuesta = await fetch(url);
    const {data} = await respuesta.json();
-    console.log(data);
+    
+
+    const gifs = data.map( imagen =>(
+      {
+        id: imagen.id,
+        title: imagen.title,
+        url: imagen.url
+
+      }
+    )
+
+    )
+    console.log(gifs);
   }
 
   getGifs();
